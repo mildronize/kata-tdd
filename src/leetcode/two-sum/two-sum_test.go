3fffa80a1bf5eaf4b69c2fc09f2e8d5ccaa69db1
Thada Wangthammang <thada.wth@gmail.com>
2022-03-20 16:54:34 +0700

    add table test driven

--- src/leetcode/two-sum/two-sum_test.go
+++ src/leetcode/two-sum/two-sum_test.go
@@ -1,3 +1,5 @@
+// Ref: https://medium.com/@pliutau/table-driven-tests-in-go-5d7e230681da
+
 package two_sum
 
 import (
@@ -6,9 +8,28 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var tests = []struct {
+	title    string
+	nums     []int
+	target   int
+	expected []int
+}{
+	{"simple", []int{2, 7, 11, 15}, 9, []int{0, 1}},
+}
+
+// func TestTwoSum(t *testing.T) {
+// 	nums := []int{2, 7, 11, 15}
+// 	target := 9
+// 	expected := []int{0, 1}
+// 	assert.ElementsMatch(t, twoSum(nums, target), expected)
+// }
+
 func TestTwoSum(t *testing.T) {
-	nums := []int{2, 7, 11, 15}
-	target := 9
-	expected := []int{0, 1}
-	assert.ElementsMatch(t, twoSum(nums, target), expected)
+	for _, tt := range tests {
+		t.Run(tt.title, func(subtest *testing.T) {
+			subtest.Parallel()
+			actual := twoSum(tt.nums, tt.target)
+			assert.ElementsMatch(subtest, actual, tt.expected)
+		})
+	}
 }
