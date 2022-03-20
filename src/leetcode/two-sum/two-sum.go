5aaf67dcc325afcf2ab37e549f9634f5def69682
Thada Wangthammang <thada.wth@gmail.com>
2022-03-20 18:03:02 +0700

    done two-sum

--- src/leetcode/two-sum/two-sum.go
+++ src/leetcode/two-sum/two-sum.go
@@ -6,5 +6,12 @@ Author: Thada Wangthammang
 package two_sum
 
 func twoSum(nums []int, target int) []int {
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
 	return []int{0, 1}
 }
--- src/leetcode/two-sum/two-sum_test.go
+++ src/leetcode/two-sum/two-sum_test.go
@@ -15,6 +15,8 @@ var tests = []struct {
 	expected []int
 }{
 	{"2+7=9", []int{2, 7, 11, 15}, 9, []int{0, 1}},
+	{"2+4=6", []int{3, 2, 4}, 6, []int{1, 2}},
+	{"3+3=6", []int{3, 3}, 6, []int{0, 1}},
 }
 
 // func TestTwoSum(t *testing.T) {
