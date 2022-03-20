21dab1f312214c8f9c1739fad09355b9f4513fca
Thada Wangthammang <thada.wth@gmail.com>
2022-03-20 21:55:08 +0700

    add template

---
+++ src/leetcode/_Template/_Template.go
@@ -0,0 +1,11 @@
+/*
+Problem: https://leetcode.com/problems/
+Author: Thada Wangthammang
+Speed:
+*/
+
+package _Template
+
+func isValid(s string) bool {
+	return true
+}
---
+++ src/leetcode/_Template/_Template_test.go
@@ -0,0 +1,25 @@
+package _Template
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var tests = []struct {
+	name     string
+	input    string
+	expected bool
+}{
+	{"Name of test Case", "input_string", true},
+}
+
+func Test_Template(t *testing.T) {
+	for _, test := range tests {
+		t.Run(test.name, func(subtest *testing.T) {
+			// subtest.Parallel()
+			actual := isValid(test.input)
+			assert.Equal(subtest, actual, test.expected)
+		})
+	}
+}
