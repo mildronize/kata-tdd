1d62b36a09778ae2fc9c016b8b2ebb0ebae13bc1
Thada Wangthammang <thada.wth@gmail.com>
2022-03-20 18:09:33 +0700

    init ValidParentheses

---
+++ src/leetcode/ValidParentheses/ValidParentheses.go
@@ -0,0 +1,9 @@
+/*
+Problem: https://leetcode.com/problems/valid-parentheses/
+Author: Thada Wangthammang
+*/
+package ValidParentheses
+
+func isValid(s string) bool {
+	return true
+}
---
+++ src/leetcode/ValidParentheses/ValidParentheses_test.go
@@ -0,0 +1,30 @@
+/*
+Problem: https://leetcode.com/problems/valid-parentheses/
+Author: Thada Wangthammang
+*/
+
+package ValidParentheses
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var tests = []struct {
+	name     string
+	input    string
+	expected bool
+}{
+	{"Simple {}", "{}", true},
+}
+
+func TestTwoSum(t *testing.T) {
+	for _, test := range tests {
+		t.Run(test.name, func(subtest *testing.T) {
+			subtest.Parallel()
+			actual := isValid(test.input)
+			assert.Equal(subtest, actual, test.expected)
+		})
+	}
+}
